c5796159c4d0c2610aed95dfe6eed1e97a30ab64
Nsika Nxumalo <nxumalonsika83@gmail.com>
2026-09-18 13:31:19 +0200

    Add files via upload

---
+++ chap01/Page 20.js
@@ -0,0 +1,7 @@
+2000 - 37;
+30*27;
+810
+120/20;
+6
+"Your name" + " " + "is learning JavaScript";
+'Your name is learning JavaScript!'
---
+++ chap01/Page 7 .js
@@ -0,0 +1,8 @@
+2000 - 37;
+1963
+30*27;
+810
+120/20;
+6
+"Emily" + " " + " is learning JavaScript! ";
+' Emily is learning JavaScript '
